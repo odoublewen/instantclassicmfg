2a0c30ccb15d6fe55dca3c051cc81e602b34fb48
Owen Solberg <owen.solberg@gmail.com>
2025-05-03 13:58:53 -0700

    Added Miscellaneous #145

--- src/icm/cli.py
+++ src/icm/cli.py
@@ -35,13 +35,17 @@ logging.config.dictConfig(config)
 
 
 DATA_FILE = Annotated[Path, typer.Argument(help="Path to yaml data file")]
-USERNAME = Annotated[str, typer.Option(envvar="SPOTIFY_USERNAME")]
+USERNAME = Annotated[str, typer.Option(help="Spotify Username", envvar="SPOTIFY_USERNAME")]
+DRYRUN = Annotated[bool, typer.Option(help="Search for tracks but do not actually make playlist")]
+MIN_HIT_RATE = Annotated[float, typer.Option(help="Fraction of hits required to make playlist")]
 
 @cli.command()
-def make_playlists(data_file: DATA_FILE, username: USERNAME):
+def make_playlists(data_file: DATA_FILE, username: USERNAME, min_hit_rate: MIN_HIT_RATE=0.7, dryrun: DRYRUN=False):
     tape_list = yaml.load(data_file)
-    MixTapeBuilder(username).process_tape_list(tape_list)
-    yaml.dump(tape_list, data_file)
+    MixTapeBuilder(username).process_tape_list(tape_list, min_hit_rate, dryrun)
+
+    if not dryrun:
+        yaml.dump(tape_list, data_file)
 
 if __name__ == "__main__":
     cli()
--- src/icm/mix_tapes.py
+++ src/icm/mix_tapes.py
@@ -29,24 +29,28 @@ class MixTapeBuilder:
         artist = tape.get('artist') or track_tuple[1].strip()
         return self._find_spotify_track(title, artist)
 
-    def _process_mix_tape(self, tape):
+    def _process_mix_tape(self, tape, min_hit_rate, dryrun):
         LOG.info("Processing mix tape {id}: {title}".format(**tape))
         tracks = tape["side_a"] + tape["side_b"]
         spotify_tracks = [self._process_track(track, tape) for track in tracks]
         spotify_tracks = list(filter(None, spotify_tracks))
 
-        if len(spotify_tracks) < len(tracks) * 0.7:
+        if len(spotify_tracks) < len(tracks) * min_hit_rate:
             LOG.warning(f"Aborting playlist creation because only {len(spotify_tracks)} of {len(tracks)} tracks found")
 
         new_playlist_name = "{title} - ICM-{id}".format(**tape)
-        playlist = self.sp.user_playlist_create(self.username, name=new_playlist_name)
-        self.sp.user_playlist_add_tracks(self.username, playlist["id"], tracks=spotify_tracks)
-        tape["spotify_playlist"] = playlist["id"]
-        LOG.info(f"Created playlist '{new_playlist_name}' with {len(spotify_tracks)} tracks")
 
-    def process_tape_list(self, tape_list):
+        if dryrun:
+            LOG.info(f"Would create playlist '{new_playlist_name}' with {len(spotify_tracks)} tracks")
+        else:
+            playlist = self.sp.user_playlist_create(self.username, name=new_playlist_name)
+            self.sp.user_playlist_add_tracks(self.username, playlist["id"], tracks=spotify_tracks)
+            tape["spotify_playlist"] = playlist["id"]
+            LOG.info(f"Created playlist '{new_playlist_name}' with {len(spotify_tracks)} tracks")
+
+    def process_tape_list(self, tape_list, min_hit_rate, dryrun):
         for tape in tape_list:
             if "spotify_playlist" in tape:
                 LOG.info("Skipping mix tape {id}: {title} (already has playlist {spotify_playlist}".format(**tape))
                 continue
-            self._process_mix_tape(tape)
+            self._process_mix_tape(tape, min_hit_rate, dryrun)
